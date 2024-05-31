3310654673d22f7b389d2c6cfeb358b83b35a2a5
Azmain Biswas <82164368+AzmainBiswas@users.noreply.github.com.>
2024-06-01 02:03:58 +0530

    installed arcolinux again

--- setup/arch.sh
+++ setup/arch.sh
@@ -4,12 +4,16 @@
 # import common variable and functions
 . ./init.sh
 
-sudo pacman -S --needed git stow starship zoxide fd ripgrep eza bash-completion \
+sudo pacman -S --needed --quite git stow starship fd ripgrep eza bash-completion \
     tmux xclip xsel trash-cli curl wget aria2 fastfetch sxiv feh libva-mesa-driver mesa-vdpau libvdpau-va-gl libva-vdpau-driver 
 
 printf "\n"
 # install fzf
 install_fzf || printf "Some thing went wrong. check your internet.\n"
+
+printf "\nInstalling zoxide\n"
+# install zoxide
+install_zoxide
 
 printf "\n"
 #install tmux tpm
