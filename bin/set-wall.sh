10b287d9644b4bbe26a4f2287769cb8103064bf2
Azmain Biswas <82164368+AzmainBiswas@users.noreply.github.com.>
2025-07-30 23:40:29 +0530

    Wed Jul 30 11:40:29 PM IST 2025

--- bin/set-wall.sh
+++ bin/set-wall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cp -f "$1" $HOME/.config/background
-killall swaybg 
+pkill swaybg 
 swaybg -m fill -i $HOME/.config/background &
 notify-send -i $HOME/.config/background "WallPaper set!!" "Wallpaper has been set to $1" &
