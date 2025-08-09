62f69f203fd1894d942a7fa703fb350eb50aa733
Azmain Biswas <82164368+AzmainBiswas@users.noreply.github.com.>
2025-08-09 23:40:02 +0530

    change in debian setup script

--- setup/debian.sh
+++ setup/debian.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
+set -ex
+
 # for all debian distro
 
 # import common variable and functions
 . ./init.sh
 
-sudo apt install git stow exa bat curl wget aria2\
+sudo apt install git stow eza bat curl wget aria2\
     ripgrep fd-find bash-completion tmux \
-    xclip xsel sxiv feh pipx
+    xclip xsel sxiv feh pipx zoxide fzf
 
 pipx install trash-cli
 pipx install yt-dlp
@@ -17,10 +19,10 @@ ln -s $(which fdfind) ~/.local/bin/fd
 ln -s $(which batcat) ~/.local/bin/bat
 
 # install fzf
-install_fzf
+# install_fzf
 
 # install zoxide
-install_zoxide
+# install_zoxide
 
 # install starship
 install_starship
